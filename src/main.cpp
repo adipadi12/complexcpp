58958c43097f466f367e38ffda3cd64abd8ae254
Aditya Jha <ajha4436@gmail.com>
2026-07-09 12:23:55 +0530

    fixed CMake to enable only src compilation and cleared headers

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 #include "math.h"
+#include "subtract.h"
 #include <iostream>
 using namespace std;
 
 int main() {
   cout << "Hello my name is adi" << endl;
-  cout << "Sum of 2 and 3 is: " << add(2, 3) << endl;
+  cout << "Sum of 2 and 3 is: " << subtract(2, 3) << endl;
+  cout << "Difference of 2 and 3 is: " << add(2, 3) << endl;
   std::cout << __cplusplus << std::endl;
   return 0;
 }
