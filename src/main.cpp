41e58e067eedfd6f26aa6758f728bc1bb5e576ef
Aditya Jha <ajha4436@gmail.com>
2026-08-12 19:49:56 +0530

    completed chapter 3

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,26 @@
 #include "../include/io.h"
+#include <iostream>
 
+void printValue(int value){
+    std::cout<<value<< '\n';
+}
 int main()
 {
-    int x { readNumber() };
-    int y { readNumber() };
-    writeAnswer(x,y);
+    // int z { readNumber() };
+    // int y { readNumber() };
+    // writeAnswer(z,y);
+    // std::cout << "kill yourself" << '\n';
+    printValue(1);
+    printValue(2);
+    printValue(5);
+    int x{ 1 };
+    std::cout << x << ' ';
+
+    x=x+2;
+    std::cout << x << ' ';
+
+    x=x+3;
+    std::cout << x << ' ';
+
     return 0;
 }
